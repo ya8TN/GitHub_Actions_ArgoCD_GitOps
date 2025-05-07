358c8830c4832fc7efc2f084a47b92f325851bba
iQuant <iquantconsult@gmail.com>
2025-05-07 01:30:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/hello', (req, res) => {
-  res.send('Hello, World! Welcome to iQuant YouTube Channel. \n');
+  res.send('Hello, World! Welcome to iQuant YouTube Channel.\n');
 });
 
 app.listen(port, () => {
